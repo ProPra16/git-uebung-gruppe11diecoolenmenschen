061ec8a298a956f8597609458c71dd45ed149871
renato56 <renato.vukovic@uni-duesseldorf.de>
2016-06-15 13:41:40 +0200

    button3 geschrieben

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,9 +18,13 @@ public class FiveButtons extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/five_buttons" +
+        GridPane root = FXMLLoader.load(getClass().getResource("/five_buttons" +
                 ".fxml"));
-
+        Button button3 = new Button("button3");
+        button3.setOnAction(event -> {
+            ((Button)(event.getSource())).getScene().getWindow().hide();
+        });
+        root.add(button3, 1, 2);
         primaryStage.setTitle("Five Buttons");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
